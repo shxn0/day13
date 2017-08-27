cdc1d71e61eba4290265a37819b68b5528b5bd4b
Shinnosuke Yoshizawa <pokopanic19@gmail.com>
2017-08-27 15:00:47 +0900

    20170827

--- app/controllers/poems_controller.rb
+++ app/controllers/poems_controller.rb
@@ -42,7 +42,7 @@ class PoemsController < ApplicationController
   def update
     respond_to do |format|
       if @poem.update(poem_params)
-        format.html { redirect_to get_poems_path, notice: 'Poem was successfully updated.' }
+        format.html { redirect_to poems_path, notice: 'Poem was successfully updated.' }
         format.json { render :show, status: :ok, location: @poem }
       else
         format.html { render :edit }
@@ -56,7 +56,7 @@ class PoemsController < ApplicationController
   def destroy
     @poem.destroy
     respond_to do |format|
-      format.html { redirect_to get_poems_path, notice: 'Poem was successfully destroyed.' }
+      format.html { redirect_to poems_path, notice: 'Poem was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,3 @@
 Rails.application.routes.draw do
-  resources :poems ,only: [:show, :update, :destroy, :edit, :new, :create]
-  get 'get/poems', to: 'poems#index'
+  resources :poems ,only: [:index, :show, :update, :destroy, :edit, :new, :create]
 end
